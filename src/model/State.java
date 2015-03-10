1af0d5bc1214187fdb3cc34efce99ac102459a19
wjpjw <thisisinvoker@gmail.com>
2015-03-10 19:02:29 +0800

    ~~

--- src/model/State.java
+++ src/model/State.java
@@ -6,15 +6,3 @@ public class State{
 }
 
 
-//每个函数前的注释格式改成下面这种
-/*
-* Full >> push(size==MAX_INT) >> OF 
-* Empty >> push() >> Full
-* */
-
-/*
- *  Full --> push(size==MAX_INT)--> OF 
- *  这里Full为preState，OF为postState
- *  size==MAX_INT为condition。
- *  
- */
