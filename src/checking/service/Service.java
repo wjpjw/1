b74040f1d24ac56a7db94dac6758c5f6821ad22b
hy <hy@114.212.241.248>
2015-04-28 09:29:45 +0800

    zzzzzz

---
+++ src/checking/service/Service.java
@@ -0,0 +1,92 @@
+package checking.service;
+
+import java.util.ArrayList;
+import model.Defect;
+import model.State;
+import model.Statechart;
+import model.Transition;
+
+public class Service {
+
+	private static Service service = new Service();
+
+	private Service() {
+
+	}
+
+	public static Service getInstance() {
+		return service;
+	}
+
+	public ArrayList<Defect> check(Statechart stateChart) {
+
+		if (stateChart == null)
+			return null;
+
+		ArrayList<Transition> tl = stateChart.getTransitions();
+		int len = tl.size();
+		ArrayList<State> list1 = new ArrayList<State>(); //可达状态集
+		ArrayList<State> list2 = stateChart.getStates(); //不可达状态集
+		ArrayList<State> list3 = stateChart.getStates(); //无法到达其他状态集
+		ArrayList<Defect> defects = new ArrayList<Defect>();
+
+		for (int i = 0; i < len; i++) {
+			Transition transition = tl.get(i);
+			if (transition.getPreState() != null
+					&& transition.getPreState().isIs_init()) {
+				list1.add(transition.getPreState());
+				list1.add(transition.getPostState());
+				for (int j = 0; j < len; j++) {
+					if (transition.getPostState().equals(
+							tl.get(j).getPreState())) {
+						transition = tl.get(j);
+						list1.add(transition.getPostState());
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < list3.size(); i++) {
+			State state = list3.get(i);
+			if (list1.contains(state)) {
+				list2.remove(state);
+			}
+		}
+
+		for (int i = 0; i < len; i++) {
+			Transition transition = tl.get(i);
+			if (transition.getPreState() != null
+					&& transition.getPostState() != null) {
+				if (!transition.getPreState().isIs_exception()) {
+					if (list3.contains(transition.getPreState()))
+						list3.remove(transition.getPreState());
+				} else if (transition.getPostState().isIs_exception()) {
+					if (list3.contains(transition.getPreState()))
+						list3.remove(transition.getPreState());
+				}
+
+			}
+		}
+
+		for (int i = 0; i < list2.size(); i++) {
+			
+			String description = "此状态不可达，对应函数规格可能存在错误！";
+			Defect defect = new Defect();
+			defect.setRelatedState(list2.get(i));
+			defect.setDescription(description);
+			defects.add(defect);
+
+		}
+
+		for (int i = 0; i < list3.size(); i++) {
+			
+			String description = "此状态无法到达其他任何普通状态！";
+			Defect defect = new Defect();
+			defect.setRelatedState(list3.get(i));
+			defect.setDescription(description);
+			defects.add(defect);
+		}
+
+		return defects;
+	}
+}
